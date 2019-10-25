c8ebc940573a7f2d83a89fbf94ad3937e2977e5f
Matheus Timbó <matheusfild4@hotmail.com>
2019-10-25 00:08:22 -0300

    Configurando firebase e tela de cadastro

---
+++ ShowDoMilhao/Pods/Target Support Files/Pods-ShowDoMilhao/Pods-ShowDoMilhao-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ShowDoMilhao : NSObject
+@end
+@implementation PodsDummy_Pods_ShowDoMilhao
+@end
--- ShowDoMilhao/ShowDoMilhao/ViewController.swift
+++ ShowDoMilhao/ShowDoMilhao/ViewController.swift
@@ -7,12 +7,72 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
+    
+    var handle: AuthStateDidChangeListenerHandle?
 
+    @IBOutlet weak var nomeInput: UITextField!
+    
+    @IBOutlet weak var emailInput: UITextField!
+    
+    @IBOutlet weak var senhaInput: UITextField!
+    
+    
+    @IBOutlet weak var avisoErro: UILabel!
+    
+    @IBAction func cadastrarClick(_ sender: Any) {
+        
+        self.avisoErro.text = ""
+        if(self.nomeInput.text != "" && self.emailInput.text != "" && self.senhaInput.text != ""){
+            
+            //fazer o login do firebase
+            let email = emailInput.text!
+            let password = senhaInput.text
+            
+            Auth.auth().createUser(withEmail: email, password: password!) {
+                authResult, error in
+                guard let user = authResult?.user, error == nil else {
+                    print(error!.localizedDescription)
+                    self.avisoErro.text = error!.localizedDescription
+                  return
+                }
+                print("\(user.email!) created")
+            }
+            
+        }else{ //ta faltando preencher algum dos campos
+            if(self.nomeInput.text == ""){
+                self.avisoErro.text = "Insira um nome"
+            } else
+                if(self.emailInput.text == ""){
+                    self.avisoErro.text = "Insira uma email"
+                }else{
+                    if(self.senhaInput.text == ""){
+                        self.avisoErro.text = "Insira uma senha"
+                    }
+            }
+        }
+        
+        
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+       
+        handle = Auth.auth().addStateDidChangeListener { (auth, user) in
+          // ...
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        Auth.auth().removeStateDidChangeListener(handle!)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
 
